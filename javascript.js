3505362fa10ba877b09b91e64bbf447012a09d3b
Caroline <thebine13@gmail.com>
2023-04-26 14:31:17 +0200

    Basis elementer og animation test

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function movetoside() {
+    let WHAT = document.getElementById("WHAT");
+    WHAT.classList.add("animation1");
+
+    let HOW = document.getElementById("HOW");
+    HOW.classList.add("animation2");
+
+    let WHY = document.getElementById("WHY");
+    WHY.classList.add("animation3");
+}
