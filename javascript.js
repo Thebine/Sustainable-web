dd89dbc66572965c94a2b29b3ac344ac71e54c16
Caroline <thebine13@gmail.com>
2023-05-03 12:09:50 +0200

    Så er der tekst! ..men animationerne er fucked..

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
+
 function movetoside() {
     let WHAT = document.getElementById("WHAT");
     WHAT.classList.add("animation1");
+
+    let whattext = document.getElementById("whattext")
+    whattext.style.display = "block";
 
     let HOW = document.getElementById("HOW");
     HOW.classList.add("animation2");
